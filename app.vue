ee4180c3adbd81c6abfd3c50a0243b06c0598df6
Christian Ramirez <c@withoutbrand.studio>
2025-10-22 13:48:39 -0500

    Cambio de layout de form

--- app.vue
+++ app.vue
@@ -16,16 +16,15 @@
         </div>
 
         <!--Form-->
-        <form action="https://submit-form.com/0I5x40oY1" class="flex flex-col space-y-4 font-secondary text-neutral-800  [&_input]:text-sm  [&_input]:outline-0 [&_input]:h-[40px] [&_input]:w-[190px] [&_input]:px-4 [&_input]:sm:w-[260px]">
+        <form action="https://submit-form.com/0I5x40oY1" class="flex flex-col space-y-4 font-secondary text-neutral-800  [&_input]:text-sm  [&_input]:outline-0 [&_input]:h-[40px] [&_input]:w-[190px] [&_input]:px-4 [&_input]:sm:w-full">
           <div class="grid grid-cols-2 gap-4 sm:grid-cols-1">
             <input v-model="document.name" type="text" placeholder="Name" required name="name">
             <input v-model="document.lastName" type="text" placeholder="Last Name" required name="lastName">
           </div>
 
-          <div class="grid grid-cols-2 gap-4 sm:grid-cols-1">
-            <input v-model="document.email" type="email" placeholder="Email" required name="email">
-            <input v-model="document.instagram" type="text" placeholder="Instagram Username" required name="instagram">
-          </div>
+          <input v-model="document.email" class="!w-full"  type="email" placeholder="Email" required name="email">
+          <input v-model="document.instagram" class="!w-full" type="text" placeholder="Instagram Username" required name="instagram">
+
           <button class="bg-primary px-4 h-[40px] w-full font-secondary text-white font-light text-sm !mt-8">
             SEND
           </button>
